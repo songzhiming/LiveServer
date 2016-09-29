a6f9bb1f71e1db2eb11c1a301125bd9d7875ae57
宋志明 <15117951262@163.com>
2016-09-29 13:50:15 +0800

    添加 直播code存储

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+//数组
+var arr = [];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,11 +14,36 @@ router.get('/', function(req, res, next) {
 router.get('/action=getLiveCode', function(req, res, next) {
   console.log(req.originalUrl);
   var timestamp=new Date().getTime();
+  console.log(arr);
+  arr.push(timestamp);
+  console.log(arr);
   var dic = {"code":0,
              "liveCode":timestamp};
   res.send(dic);
 });
 
-
+router.get('/action=deleteLiveCode', function(req, res, next) {
+  var dic = req.query;
+  console.log(dic.code)
+  console.log(arr)
+  arr.remove(dic.code)
+  console.log(arr)
+  var dic = {"code":0};
+  res.send(dic);
+});
 
 module.exports = router;
+/*
+ * 删除数组中指定值
+ */
+Array.prototype.remove=function(value){
+  var len = this.length;
+  for(var i=0,n=0;i<len;i++){//把出了要删除的元素赋值给新数组
+    if(this[i]!=value){
+      this[n++]=this[i];
+    }else{
+      console.log(i);//测试所用
+    }
+  }
+  this.length = n;
+};
